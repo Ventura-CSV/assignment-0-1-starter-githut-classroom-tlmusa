6a5875e845398d69e4d23cdf72a6c07fb9dc5092
tlmusa <thalia_murguiasanto1@my.vcccd.edu>
2023-08-20 00:31:10 -0700

    Create main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ def main():
     # make your code below
     # print('Hello World')
     ##############################
-
+    print ('Hello World')
+    print ('CS: Python Programming')
 
 if __name__ == '__main__':
     main()
